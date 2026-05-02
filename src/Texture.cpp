83840917deee1cd3041ed285bc14ea40577f8c3e
Altaiir1 <lemi25yuerekli@gmail.com>
2026-05-02 20:24:28 +0200

    [TASK]: FInalize Texture.cpp class with temporary hardcoded 2D Texture

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -15,38 +15,62 @@ namespace trace
 			throw std::runtime_error(std::string("Failed to load texture: ") + path);
 
 		glGenTextures(1, &m_id);
-		glBindTexture(GL_TEXTURE_3D, m_id);
+		glBindTexture(GL_TEXTURE_2D, m_id);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);					// horizontal wrapping (S = X axis in UV space)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);					// Vertical	  wrapping (T = Y axis in UV space)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // Filtering when texture is smaller than screen
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);				// Filtering when texture is larger than screen
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB, m_width, m_height, 0, GL_SRGB, GL_UNSIGNED_BYTE, data);
+		GLenum format = GL_RGB;
+		if (nrChannels == 1) format = GL_RED;
+		if (nrChannels == 3) format = GL_RGB;
+		if (nrChannels == 4) format = GL_RGBA;
+
+		glTexImage2D(GL_TEXTURE_2D, 0, format, m_width, m_height, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 
 		stbi_image_free(data);
+
+		Logger::info("[Texture] Loaded: {} ({}x{} channels:{})", path, m_width, m_height, nrChannels);
 	}
 
 	Texture::~Texture()
 	{
-
+		if (m_id)
+			glDeleteTextures(1, &m_id);
 	}
 
 	Texture::Texture(Texture&& other) noexcept
+		: m_id(other.m_id)
+		, m_width(other.m_width)
+		, m_height(other.m_height)
 	{
+		other.m_id = 0;
 	}
 
 	Texture& Texture::operator=(Texture&& other) noexcept
 	{
-		// TODO: insert return statement here
+		if (this != &other)
+		{
+			if (m_id)
+				glDeleteTextures(1, &m_id);
+			m_id = other.m_id;
+			m_width = other.m_width;
+			m_height = other.m_height;
+			other.m_id = 0;
+		}
+		return *this;
 	}
 
 	void Texture::bind(U32 slot) const
 	{
+		glActiveTexture(GL_TEXTURE0 + slot); // Through GL_TEXTURE31
+		glBindTexture(GL_TEXTURE_2D, m_id);
 	}
 
 	void Texture::unbind() const
 	{
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 }
