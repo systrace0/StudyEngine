474e9679e36eb87a5890c5acbe3b6cf0aecf304d
Altaiir1 <lemi25yuerekli@gmail.com>
2026-05-02 22:20:36 +0200

    [WIP]: Include GLM via fetchcontent and updae Dependencies.cmake & CmakeLists.txt

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -32,3 +32,12 @@ FetchContent_Declare(
 # Prevent overriding parent project's compiler/linker settings on Windows
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
+
+# GLM
+FetchContent_Declare(
+    glm
+    GIT_REPOSITORY	https://github.com/g-truc/glm.git
+    GIT_TAG 	1.0.3
+    GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(glm)
